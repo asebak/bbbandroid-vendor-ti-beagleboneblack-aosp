d03f7a68f47f69a7787f47c6bcf474b5e00a48d1
Ahmad <asebak@users.noreply.github.com>
2016-01-22 14:03:36 -0800

    Create  bdroid_buildcfg.h

---
+++ hardware/bluetooth/ bdroid_buildcfg.h
@@ -0,0 +1,15 @@
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+
+#define BTM_DEF_LOCAL_NAME    "BBB-BT"
+
+/*USB Dongle*/
+
+#define BLUETOOTH_HCI_USE_USB   TRUE
+
+/* BLE */
+#define BTA_GATT_INCLUDED  TRUE
+#define BLE_INCLUDED       TRUE
+#define SMP_INCLUDED       TRUE
+
+#endif
